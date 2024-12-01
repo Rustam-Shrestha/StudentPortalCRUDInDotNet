74bc9d09f15a8337fa0ca5bd816376d3014a3d17
Santosh <santosh.787402@smc.tu.edu.np>
2024-12-01 17:10:11 +0545

    these files missed as it was supposed to get along with previous commits

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -2,32 +2,28 @@
 using StudentPortal.Models;
 using StudentPortal.Data;
 using StudentPortal.Models.Entities;
-using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
-using Microsoft.AspNetCore.Mvc.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
 namespace StudentPortal.Controllers
 {
     public class StudentsController : Controller
     {
-        //constructor assign in application dbcontext 
-        //ApplicationDbContext is Like a container that has a tuple for each row in database
+        // Constructor to assign ApplicationDbContext
         public ApplicationDbContext dbcontext;
         public StudentsController(ApplicationDbContext dbcontext)
         {
             this.dbcontext = dbcontext;
         }
 
-
-        //displaying the registration form 
+        // Displaying the registration form
         [HttpGet]
         public IActionResult AddStudent()
         {
             return View();
         }
-        //actual form data taker with post request
+
+        // Handling form data with POST request
         [HttpPost]
-        //asynchronously performing post request that takes mod as parameter
-        //mod is literally the data we got from the form they filled mod 
-            //basically encaplustes them and brings it
         public async Task<IActionResult> AddStudent(AddStudentViewModel mod)
         {
             var student = new Student
@@ -37,12 +33,59 @@ namespace StudentPortal.Controllers
                 Subscribed = mod.Subscribed,
                 Email = mod.Email
             };
-            //asynchronously add the data
+            // Asynchronously add the data
             await dbcontext.Students.AddAsync(student);
-            //save the data
+            // Save the data
             await dbcontext.SaveChangesAsync();
-            //After saving the data to the database display the form as it is
+            // After saving the data to the database, display the form as it is
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> StudentsList()
+        {
+            var listedStudents = await dbcontext.Students.ToListAsync();
+            return View(listedStudents);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateList(Guid id)
+        {
+            // Asynchronously finding the data
+            var foundStudent = await dbcontext.Students.FindAsync(id);
+            return View(foundStudent);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateList(Student viewmodel)
+        {
+            // Asynchronously finding the data
+            var foundStudent = await dbcontext.Students.FindAsync(viewmodel.Id);
+            if (foundStudent is not null)
+            {
+                // Reassigning the tuples or found ones as something from viewmodel (updated data)
+                foundStudent.Name = viewmodel.Name;
+                foundStudent.Phone = viewmodel.Phone;
+                foundStudent.Subscribed = viewmodel.Subscribed;
+                foundStudent.Email = viewmodel.Email;
+
+                await dbcontext.SaveChangesAsync();
+            }
+            // Returning the value in list format to the students controller
+            return RedirectToAction("StudentsList", "Students");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteStudent(Student viewmodel)
+        {
+            // Asynchronously finding the data
+            var foundStudent = await dbcontext.Students.FindAsync(viewmodel.Id);
+            if (foundStudent is not null)
+            {
+                dbcontext.Students.Remove(foundStudent);
+                await dbcontext.SaveChangesAsync();
+            }
+            return RedirectToAction("StudentsList", "Students");
         }
     }
 }
